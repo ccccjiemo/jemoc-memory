d64172700bc742c9de2ea185a3402039d3e3e7ba
ccccjiemo <631003561@qq.com>
2025-01-23 13:44:30 +0800

    修改ms缓冲，修复zip entry移除错误

---
+++ jemoc_stream/src/main/cpp/include/xml/XmlReader.h
@@ -0,0 +1,10 @@
+//
+// Created on 2025/1/22.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef JEMOC_STREAM_TEST_XMLREADER_H
+#define JEMOC_STREAM_TEST_XMLREADER_H
+
+#endif //JEMOC_STREAM_TEST_XMLREADER_H
